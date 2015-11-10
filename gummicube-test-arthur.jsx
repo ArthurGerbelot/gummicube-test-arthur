e9a9ca668b7a8c94470adb43bb4d4f17b16cda45
Arthur G <arthur.gerbelot@gmail.com>
2015-11-10 17:44:57 -0500

    Add 5sec timeout for request

--- gummicube-test-arthur.jsx
+++ gummicube-test-arthur.jsx
@@ -2,14 +2,14 @@ if (Meteor.isServer) {
 
   Meteor.methods({
     makeRequest: function (url) {
-      return HTTP.call("GET", url);
+      return HTTP.call("GET", url, {timeout: 5000}); // max 5sec
     }
   });
 }
 
 if (Meteor.isClient) {
   // This code is executed on the client only
- 
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
